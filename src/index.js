fa9d2a00560c800320162b6c0b29ed61aaeb0591
Simon Seebacher <sseebacher.mmt-b2018@fh-salzburg.ac.at>
2020-01-09 00:32:17 +0100

    add burger, basic structure and some content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import "./styles/main.scss";
-console.log("hello world!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.querySelector(".main-navigation");
+    const burger = document.querySelector(".burger");
+
+    function toggleNav() {
+        nav.classList.toggle("open");
+        burger.classList.toggle("open");
+    }
+
+    burger.addEventListener("click", toggleNav);
+
+    const links = document.querySelectorAll("header nav a");
+    links.forEach(link => link.addEventListener('click', scrollToLink));
+
+    function scrollToLink(event) {
+        event.preventDefault();
+        const navHeight = document.querySelector(".navbar").offsetHeight;
+        const offsetTop = document.querySelector(this.hash).offsetTop;
+        console.log(offsetTop);
+        window.scrollTo({
+            top: offsetTop - navHeight,
+            behavior: 'smooth'
+        });
+        window.history.pushState({}, "", this.hash);
+        toggleNav();
+    }
+});
